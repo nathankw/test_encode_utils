559da4c98dfe80f40c532f0040cce9a0c1079daa
Nathaniel Watson <nathankw@DN0a22f436.SUNet>
2018-04-27 12:29:27 -0700

    bob

--- encode_utils/utils.py
+++ encode_utils/utils.py
@@ -10,16 +10,6 @@
 Contains utilities that don't require authorization on the DCC servers.
 """
 
-import hashlib
-import json
-import jsonschema
-import logging
-import os
-import requests
-import subprocess
-
-import encode_utils as eu
-
 
 #: Stores the HTTP headers to indicate JSON content in a request.
 REQUEST_HEADERS_JSON = {'content-type': 'application/json'}
